e35e75d686030bf7e09a6cf6b747ce86cbd5f5fb
Praveensivarj <praveensivarj3@gmail.com>
2025-02-07 17:44:47 +0530

    Deleted unwanted files

--- circle.js
+++
@@ -1,9 +0,0 @@
-const PI = 3.14;
-
-const calculateArea = (radius)=>{
-    return PI * radius*radius;
-}
-
-module.exports = {
-    area:calculateArea
-}
--- program1.js
+++
@@ -1,3 +0,0 @@
-const circle = require('./circle');
-
-console.log("Area of circle is : ", circle.area(5));
